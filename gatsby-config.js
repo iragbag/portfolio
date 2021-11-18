e4cee173aa61bceb872e80cc43376e1a316d8451
Raghav Sharma <iragbag@raghavs-mbp-2.lan>
2021-11-18 07:02:03 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Resume",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
