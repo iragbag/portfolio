4fab7a4077362f491f6f53777b1530b3b939f1f1
Raghav Sharma <iragbag@raghavs-mbp-2.lan>
2021-11-23 07:47:18 -0500

    updating some simple errors

--- src/components/resume/previousExperience.js
+++ src/components/resume/previousExperience.js
@@ -1,29 +1,29 @@
 import * as React from 'react'
 //import { Link } from 'gatsby'
-import * as styles from '../../styles/previousExperience.module.css'
+import * as styles from '../../styles/PreviousExperience.module.css'
 
 const PreviousExperience = ({ data, index }) => {
   return (
-    <div className={styles.item}>
+    <div className={ styles.item }>
         <div className={styles.title}> 
-           <a href= {data.companyURL}> {data.position} @ { data.companyName } </a>
+           <a href= { data.companyURL }> {data.position} @ { data.companyName } </a>
             </div>
         <div className={ styles.duration }>
-            <span className={styles.durationStart}>{ data.startTime }</span> - <span className={styles.durationEnd}>{ data.endTime }</span>
+            <span className={ styles.durationStart }>{ data.startTime }</span> - <span className={styles.durationEnd}>{ data.endTime }</span>
         </div>
-        <div className={styles.highlights}>
+        <div className={ styles.highlights }>
             <ul>
-                {data.highlights.map((data,index) => 
+                { data.highlights.map((data,index) => 
                     <li key={index}>{ data }</li>
-                )}
+                ) }
             </ul>
         </div>  
-        <div className={styles.projects}>
+        <div className={ styles.projects }>
             <div className={ styles.projectsTitle }>Project Highlights:</div>
             <ul>
-                {data.projects.map((data,index) => 
+                { data.projects.map((data,index) => 
                     <li key={index}>{ data }</li>
-                )}
+                ) }
             </ul>
         </div>
     </div>
