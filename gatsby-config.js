2fd36316398398db7c1fe33a0bc559219478f05c
iragbag <irraghav7@gmail.com>
2023-01-26 09:46:52 -0500

    publishing changes to production

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Resume",
+    siteUrl: "https://www.raghavsharma.ca",
+    title: "Resume and Blog",
   },
   plugins: [
     {
